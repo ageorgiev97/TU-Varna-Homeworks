24d2f68c778f2a656a85b3c9692403db86ecc56e
Daniel Atanasov <daniel.a.atanasov97@gmail.com>
2017-03-02 00:49:47 +0200

    Коментари

--- Practice Programming/T4_SelfProgams/T4_Primer1_self/T4_Primer1_self.cpp
+++ Practice Programming/T4_SelfProgams/T4_Primer1_self/T4_Primer1_self.cpp
@@ -1,21 +1,35 @@
+// Демонстрация на оператор в класове
+//
+// Съдържа структура за обработка на цели числас оператори за присвояване,
+// събиране и изваждане, както с друг обект така и с примитивен int
+// 
+// Изготвил: Даниел Атанасов Атанасов
+// Фк. № 61662148
+// Гр. СИТ 2б
+
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
+// Използва се структура защото членовете са публични по подразбиране
 struct Integer {
+	// Стойността на числото
 	int value;
 
+	// Конструктори
 	Integer(int i):
 		value(i) { }
 
 	Integer():
 		value(0) { }
 
+	// Оператор за взимане на стойността на обекта
 	inline int operator()() const {
 		return this->value;
 	}
 
+	// Оператори за събиране на два обекта или обект и примитивен int (отговорът е в нов обект)
 	inline friend Integer operator+(Integer const &a, Integer const &b) {
 		return Integer(a.value + b.value);
 	}
@@ -24,6 +38,7 @@ struct Integer {
 		return Integer(a.value + b);
 	}
 
+	// Оператори за изваждане на два обекта или обект и примитивен int (отговорът е в нов обект)
 	inline friend Integer operator-(Integer const &a, Integer const &b) {
 		return Integer(a.value - b.value);
 	}
@@ -32,6 +47,7 @@ struct Integer {
 		return Integer(a.value - b);
 	}
 
+	// Оператори за събиране на два обекта или обект и примитивен int (като се проненя единият обект)
 	inline Integer& operator+=(Integer const &a) {
 		this->value += a.value;
 		return *this;
@@ -42,6 +58,7 @@ struct Integer {
 		return *this;
 	}
 
+	// Оператори за изваждане на два обекта или обект и примитивен int (като се проненя единият обект)
 	inline Integer& operator-=(Integer const &a) {
 		this->value -= a.value;
 		return *this;
@@ -52,6 +69,7 @@ struct Integer {
 		return *this;
 	}
 
+	// Оператори за присвояване на (стойността на) друг обект или на примитивен int
 	inline Integer& operator=(int const &a) {
 		this->value = a;
 		return *this;
@@ -64,30 +82,39 @@ struct Integer {
 };
 
 int main() {
-	Integer i(0);
-	cout << "Integer i = 0" << endl;
+	// Примери
+	
+	// По подразбиране, стойността е 0
+	Integer i;
+	cout << "Integer i" << endl;
 	cout << "i() = " << i() << endl << endl;
 
+	// Събиране и присвояване
 	i += 10;
 	cout << "i += 10" << endl;
 	cout << "i() = " << i() << endl << endl;
 
+	// Изваждане и присвояване
 	i -= 9;
 	cout << "i -= 9" << endl;
 	cout << "i() = " << i() << endl << endl;
 
+	// Присвояване
 	i = 8;
 	cout << "i = 8" << endl;
 	cout << "i() = " << i() << endl << endl;
 
+	// Събиране на два обекта и присвояване на получения сбор
 	i = Integer(10) + Integer(3);
 	cout << "i = Integer(10) + Integer(3)" << endl;
 	cout << "i() = " << i() << endl << endl;
 
+	// Събиране на обект и примитивен int и присвояване на получения сбор
 	i = 8 + Integer(3);
 	cout << "i = 8 + Integer(3)" << endl;
 	cout << "i() = " << i() << endl << endl;
 
+	// Изваждане на обект и примитивен int и присвояване на получения сбор
 	i = 6 - Integer(4);
 	cout << "i = 6 - Integer(4)" << endl;
 	cout << "i() = " << i() << endl << endl;
